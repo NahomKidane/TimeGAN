6b7076f21eca33831b6cea2579f5a5a9f2cf8409
Nahom Kidane <nkida001@gmail.com>
2025-07-16 16:21:17 -0400

    Update utils.py, Rewritten TF2.x-Compatible

--- utils.py
+++ utils.py
@@ -1,145 +1,92 @@
-"""Time-series Generative Adversarial Networks (TimeGAN) Codebase.
+"""
+Time-series Generative Adversarial Networks (TimeGAN) Codebase - TF2.x Version
 
-Reference: Jinsung Yoon, Daniel Jarrett, Mihaela van der Schaar, 
-"Time-series Generative Adversarial Networks," 
-Neural Information Processing Systems (NeurIPS), 2019.
+Converted from original TF1.x version (Jinsung Yoon et al., 2019).
+Maintains the same logic, but uses tf.keras & NumPy arrays for TF2 compatibility.
 
-Paper link: https://papers.nips.cc/paper/8789-time-series-generative-adversarial-networks
-
-Last updated Date: April 24th 2020
-Code author: Jinsung Yoon (jsyoon0823@gmail.com)
-
------------------------------
-
-utils.py
-
-(1) train_test_divide: Divide train and test data for both original and synthetic data.
-(2) extract_time: Returns Maximum sequence length and each sequence length.
-(3) rnn_cell: Basic RNN Cell.
-(4) random_generator: random vector generator
-(5) batch_generator: mini-batch generator
+Functions:
+(1) train_test_divide
+(2) extract_time
+(3) rnn_cell (returns string; now handled in main code)
+(4) random_generator
+(5) batch_generator
 """
 
-## Necessary Packages
 import numpy as np
 import tensorflow as tf
 
+# -----------------------------------------------------------------------------
+# 1. Train/Test Split
+# -----------------------------------------------------------------------------
+def train_test_divide(data_x, data_x_hat, data_t, data_t_hat, train_rate=0.8):
+    no = len(data_x)
+    idx = np.random.permutation(no)
+    train_idx = idx[:int(no * train_rate)]
+    test_idx = idx[int(no * train_rate):]
 
-def train_test_divide (data_x, data_x_hat, data_t, data_t_hat, train_rate = 0.8):
-  """Divide train and test data for both original and synthetic data.
-  
-  Args:
-    - data_x: original data
-    - data_x_hat: generated data
-    - data_t: original time
-    - data_t_hat: generated time
-    - train_rate: ratio of training data from the original data
-  """
-  # Divide train/test index (original data)
-  no = len(data_x)
-  idx = np.random.permutation(no)
-  train_idx = idx[:int(no*train_rate)]
-  test_idx = idx[int(no*train_rate):]
-    
-  train_x = [data_x[i] for i in train_idx]
-  test_x = [data_x[i] for i in test_idx]
-  train_t = [data_t[i] for i in train_idx]
-  test_t = [data_t[i] for i in test_idx]      
-    
-  # Divide train/test index (synthetic data)
-  no = len(data_x_hat)
-  idx = np.random.permutation(no)
-  train_idx = idx[:int(no*train_rate)]
-  test_idx = idx[int(no*train_rate):]
-  
-  train_x_hat = [data_x_hat[i] for i in train_idx]
-  test_x_hat = [data_x_hat[i] for i in test_idx]
-  train_t_hat = [data_t_hat[i] for i in train_idx]
-  test_t_hat = [data_t_hat[i] for i in test_idx]
-  
-  return train_x, train_x_hat, test_x, test_x_hat, train_t, train_t_hat, test_t, test_t_hat
+    train_x = [data_x[i] for i in train_idx]
+    test_x = [data_x[i] for i in test_idx]
+    train_t = [data_t[i] for i in train_idx]
+    test_t = [data_t[i] for i in test_idx]
 
+    no = len(data_x_hat)
+    idx = np.random.permutation(no)
+    train_idx = idx[:int(no * train_rate)]
+    test_idx = idx[int(no * train_rate):]
 
-def extract_time (data):
-  """Returns Maximum sequence length and each sequence length.
-  
-  Args:
-    - data: original data
-    
-  Returns:
-    - time: extracted time information
-    - max_seq_len: maximum sequence length
-  """
-  time = list()
-  max_seq_len = 0
-  for i in range(len(data)):
-    max_seq_len = max(max_seq_len, len(data[i][:,0]))
-    time.append(len(data[i][:,0]))
-    
-  return time, max_seq_len
+    train_x_hat = [data_x_hat[i] for i in train_idx]
+    test_x_hat = [data_x_hat[i] for i in test_idx]
+    train_t_hat = [data_t_hat[i] for i in train_idx]
+    test_t_hat = [data_t_hat[i] for i in test_idx]
 
+    return train_x, train_x_hat, test_x, test_x_hat, train_t, train_t_hat, test_t, test_t_hat
 
+# -----------------------------------------------------------------------------
+# 2. Extract Time
+# -----------------------------------------------------------------------------
+def extract_time(data):
+    """Returns time lengths and max sequence length."""
+    time = [len(seq) for seq in data]
+    max_seq_len = max(time)
+    return time, max_seq_len
+
+# -----------------------------------------------------------------------------
+# 3. RNN Cell Placeholder (TF2 uses keras layers instead)
+# -----------------------------------------------------------------------------
 def rnn_cell(module_name, hidden_dim):
-  """Basic RNN Cell.
-    
-  Args:
-    - module_name: gru, lstm, or lstmLN
-    
-  Returns:
-    - rnn_cell: RNN Cell
-  """
-  assert module_name in ['gru','lstm','lstmLN']
-  
-  # GRU
-  if (module_name == 'gru'):
-    rnn_cell = tf.nn.rnn_cell.GRUCell(num_units=hidden_dim, activation=tf.nn.tanh)
-  # LSTM
-  elif (module_name == 'lstm'):
-    rnn_cell = tf.contrib.rnn.BasicLSTMCell(num_units=hidden_dim, activation=tf.nn.tanh)
-  # LSTM Layer Normalization
-  elif (module_name == 'lstmLN'):
-    rnn_cell = tf.contrib.rnn.LayerNormBasicLSTMCell(num_units=hidden_dim, activation=tf.nn.tanh)
-  return rnn_cell
+    """
+    For compatibility with original code.
+    In TF2, we use tf.keras.layers directly in timegan.py.
+    """
+    return module_name, hidden_dim
 
+# -----------------------------------------------------------------------------
+# 4. Random Vector Generator
+# -----------------------------------------------------------------------------
+def random_generator(batch_size, z_dim, T_mb, max_seq_len):
+    Z_mb = np.zeros((batch_size, max_seq_len, z_dim))
+    for i in range(batch_size):
+        temp_Z = np.random.uniform(0., 1., (T_mb[i], z_dim))
+        Z_mb[i, :T_mb[i], :] = temp_Z
+    return Z_mb.astype(np.float32)
 
-def random_generator (batch_size, z_dim, T_mb, max_seq_len):
-  """Random vector generation.
-  
-  Args:
-    - batch_size: size of the random vector
-    - z_dim: dimension of random vector
-    - T_mb: time information for the random vector
-    - max_seq_len: maximum sequence length
-    
-  Returns:
-    - Z_mb: generated random vector
-  """
-  Z_mb = list()
-  for i in range(batch_size):
-    temp = np.zeros([max_seq_len, z_dim])
-    temp_Z = np.random.uniform(0., 1, [T_mb[i], z_dim])
-    temp[:T_mb[i],:] = temp_Z
-    Z_mb.append(temp_Z)
-  return Z_mb
-
-
+# -----------------------------------------------------------------------------
+# 5. Batch Generator (TF2 padded)
+# -----------------------------------------------------------------------------
 def batch_generator(data, time, batch_size):
-  """Mini-batch generator.
-  
-  Args:
-    - data: time-series data
-    - time: time information
-    - batch_size: the number of samples in each batch
-    
-  Returns:
-    - X_mb: time-series data in each batch
-    - T_mb: time information in each batch
-  """
-  no = len(data)
-  idx = np.random.permutation(no)
-  train_idx = idx[:batch_size]     
-            
-  X_mb = list(data[i] for i in train_idx)
-  T_mb = list(time[i] for i in train_idx)
-  
-  return X_mb, T_mb
+    no = len(data)
+    idx = np.random.permutation(no)
+    batch_idx = idx[:batch_size]
+
+    max_seq_len = max([time[i] for i in batch_idx])
+    dim = data[0].shape[-1]
+
+    X_mb = np.zeros((batch_size, max_seq_len, dim))
+    T_mb = []
+
+    for i, j in enumerate(batch_idx):
+        seq_len = time[j]
+        X_mb[i, :seq_len, :] = data[j]
+        T_mb.append(seq_len)
+
+    return X_mb.astype(np.float32), np.array(T_mb, dtype=np.int32)
